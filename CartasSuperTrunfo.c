e7e6e447d8ec2fb3614b16be8b99aecd94118d0d
Juliano <59809207+juliano-xd@users.noreply.github.com>
2025-02-17 07:35:42 -0300

    Foi adicionado o calculo altomatico do PIB per Capta e Densidade Populacional

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,13 @@
-// Feito por Juliano-xd
-
 #include <inttypes.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 
-// Macro para printar uma string com uma quebra de linha, (apenas uma facilidade).
+// Macro para printar uma string com uma quebra de linha.
 #define printl(x) printf("%s\n", x)
 
-/* Esta struct representa uma carta do jogo Super Trunfo. */
+/* Esta struct representa uma carta do jogo Super Trunfo.
+*/
 struct Carta{
     char estado;
     int codigo; // 0, 1, 3, 4
@@ -17,6 +16,8 @@ struct Carta{
     float area;
     float pib;
     int pontos_turisticos;
+    float densidade_populacional;
+    float pib_per_capita;
 };
 
 int main(){
@@ -24,10 +25,10 @@ int main(){
 
     // Preenchimento da carta.
     printl("Digite o estado (A até H):");
-    scanf("%c", &carta.estado); // Lê um caractere (o estado) , ainda nao foi implementado nenhuma verificação.
+    scanf("%c", &carta.estado); // Lê um caractere (o estado).
 
     printl("Digite o código da carta (1 até 4):");
-    scanf("%u", &carta.codigo); // Lê um inteiro sem sinal (o codigo da carta), tambem sem verificação.
+    scanf("%u", &carta.codigo); // Lê um inteiro sem sinal (o codigo).
 
     printl("Digite o nome do estado (Sem espaços):");
     scanf("%s", carta.nome); // Lê uma string (o nome da cidade).
@@ -44,13 +45,26 @@ int main(){
     printl("Digite a quantidade de pontos turísticos:");
     scanf("%u", &carta.pontos_turisticos); // Lê um inteiro sem sinal (a quantidade de pontos turísticos).
 
-    /* Estas linhas apenas printam os valores da carta */
+    /* Estas linhas apenas printam os valores da carta.
+    */
     printf("Estado: %c\n", carta.estado);
     printf("Código: %c0%u\n", carta.estado ,carta.codigo); // O código da carta é impresso com o estado.
     printf("Nome: %s\n", carta.nome);
     printf("População: %u\n", carta.populacao);
     printf("Área: %.2f Km²\n", carta.area);
+
+    // Calcula a densidade populacional.
+    // carta.densidade_populacional = carta.populacao / carta.area;
+    carta.densidade_populacional = (float)carta.populacao / carta.area;
+    printf("Densidade populacional: %.2f hab/Km²\n", carta.densidade_populacional);
     printf("PIB: %.2f bilhoes de reais\n", carta.pib);
+
+    // Calcula o PIB per capita.
+    // carta.pib_per_capita = carta.pib / carta.populacao;
+    carta.pib_per_capita = (carta.pib * 1000000000) / carta.populacao;
+    printf("PIB per capita: %.2f reais\n", carta.pib_per_capita);
+
     printf("Pontos turísticos: %u\n", carta.pontos_turisticos);
+
     return 0;
 }
